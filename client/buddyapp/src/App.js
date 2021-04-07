b5cd5a37036fee760080136b4e512ec0a353fc0f
theklaoww <kontawat.tk@mail.kmutt.ac.th>
2021-04-07 15:44:40 +0700

    XDDRFHRFH
    GFGG

--- client/buddyapp/src/App.js
+++ client/buddyapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React fuck
+          Learn React fuckVDXVBD
         </a>
       </header>
     </div>
